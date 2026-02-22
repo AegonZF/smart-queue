09d4ebeb843ad9b0766d4a5201e9f9ceac8a471b
AegonZF <pablofz2002@gmail.com>
2026-02-22 17:54:24 -0600

    Se añadio columna de roles a Tabla Users

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
